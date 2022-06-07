b79f0001862db2096ac7eebcbccb306d1312662e
Jalal-op <jalalbaloch570@gmail.com>
2022-06-07 13:17:30 +0500

    commited

--- JAVA.js
+++ JAVA.js
@@ -1,3 +1,11 @@
 $('#blogCarousel').carousel({
     interval: 5000
 });
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+$(".hover").mouseleave(
+    function() {
+      $(this).removeClass("hover");
+    }
+  );
